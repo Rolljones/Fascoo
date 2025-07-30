92d0f85e39a0f0992fcbc370436271fea1f91c14
Roland Jonathan <rolandjonathan2002@gmail.com>
2025-07-30 16:49:06 +0100

    Second commit

---
+++ js/shop.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("active");
+        burger.classList.toggle("toggle");
+    });
+});
+
+
+
+
+
+const faders = document.querySelectorAll('.fade-in');
+
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  });
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+
